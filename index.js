4caf8148ff6b08b31f7e375c3f89219871ed92dd
IoannisI123 <113583318+IoannisI123@users.noreply.github.com>
2024-03-30 12:41:25 +0200

    Set up opponent random attack and recover move

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // Constants
 const DELAY_SPECIAL_REND = 3800;
 const DELAY_DRACO_METEOR = 3800;
+const DELAY_RECOVER = 2200;
+const DELAY_RECOVERY_TEXT = 3800;
 const DELAY_THUNDER_DAMAGE = 2200;
 const DELAY_TAKE_DAMAGE = 2800;
 const DELAY_OPPONENT_ATTACK = 6000;
@@ -24,7 +26,7 @@ let disableButtons = document.getElementsByClassName("buttons").disabled = true;
 //Sound effects Audio(Dom)
 
 const hitWeak = new Audio("https://static.wikia.nocookie.net/soundeffects/images/5/50/Hit_Weak_Not_Very_Effective.mp3");
-
+const hitStrong = new Audio("https://static.wikia.nocookie.net/soundeffects/images/c/c2/Hit_Super_Effective.mp3")
 //Palkia Attacks Audio 
 
 const SpacialRendAudio = new Audio("https://epsilon.vgmsite.com/soundtracks/pokemon-sfx-gen-4-attack-moves-dppl-hg-ss/zxleqbbksw/Spacial%20Rend.mp3");
@@ -60,7 +62,7 @@ const availablePokemon = {
    },
 
    "Arceus":{
-    "moves":["Outrage","Extreme Speed","Earthquake", "Recover"],
+    "moves":["Outrage","shadowClaw","Earthquake", "Recover"],
     "buttonText": ["Fight", "Switch", "Medicine", "Escape"]
    },
 };
@@ -121,7 +123,7 @@ function spacialRend(){
   setTimeout(attackText, DELAY_SPECIAL_REND);
   setTimeout(calculateDamage, DELAY_TAKE_DAMAGE);
  // setTimeout(damageText, 4800);
-  setTimeout(earthquake, DELAY_OPPONENT_ATTACK);
+  setTimeout(randomOpAttack, DELAY_OPPONENT_ATTACK);
 
   fightbtn.classList.add("fightButtonHidden");
   switchbtn.classList.add("switchButtonHidden");
@@ -138,7 +140,7 @@ function spacialRend(){
   setTimeout(attackText, DELAY_DRACO_METEOR);
   setTimeout(calculateDamage, DELAY_TAKE_DAMAGE);
  // setTimeout(damageText, 4800);
- setTimeout(earthquake, DELAY_OPPONENT_ATTACK);
+ setTimeout(randomOpAttack, DELAY_OPPONENT_ATTACK);
 
   fightbtn.classList.add("fightButtonHidden");
   switchbtn.classList.add("switchButtonHidden");
@@ -164,7 +166,7 @@ function spacialRend(){
   setTimeout(attackText, DELAY_DRACO_METEOR);
   setTimeout(calculateDamage, DELAY_THUNDER_DAMAGE);
  // setTimeout(damageText, 4800);
- setTimeout(earthquake, DELAY_OPPONENT_ATTACK);
+ setTimeout(randomOpAttack, DELAY_OPPONENT_ATTACK);
 
   fightbtn.classList.add("fightButtonHidden");
   switchbtn.classList.add("switchButtonHidden");
@@ -190,7 +192,7 @@ function spacialRend(){
   setTimeout(attackText, DELAY_DRACO_METEOR);
   setTimeout(calculateDamage, DELAY_THUNDER_DAMAGE);
  // setTimeout(damageText, 4800);
-  setTimeout(earthquake, DELAY_OPPONENT_ATTACK);
+  setTimeout(randomOpAttack, DELAY_OPPONENT_ATTACK);
 
   fightbtn.classList.add("fightButtonHidden");
   switchbtn.classList.add("switchButtonHidden");
@@ -203,15 +205,30 @@ function spacialRend(){
 
  function calculateDamage(){
   hitWeak.play();
-  healthP1.value -= Math.floor(Math.random() * (12 - 22) + 22);
+  healthP1.value -= Math.floor(Math.random() * (22 - 30) + 30);
   healthBarP1.textContent = healthP1.value;
 }
 
-   function calculatOpDamage(){
+   function calculateOpDamage(){
 
     hitWeak.play();
-    healthP2.value -= Math.floor(Math.random() * (12 - 22) + 22);
+    healthP2.value -= Math.floor(Math.random() * (22 - 30) + 30);
     healthBarP2.textContent = healthP2.value;
+
+   }
+
+   function calculateSuperEffOpDamage(){
+
+    hitStrong.play();
+    healthP2.value -= Math.floor(Math.random() * (33 - 40) + 40);
+    healthBarP2.textContent = healthP2.value;
+
+   }
+
+  function healOpHealth(){
+
+    healthP1.value = 100;
+     healthBarP1.textContent = healthP1.value;
 
    }
 
@@ -229,8 +246,19 @@ function spacialRend(){
    
   }
 
+  function superEffOpAttackText() {
   
+    mytext.textContent = "It's Super Effective";
+   
+  }
 
+ function recoverHealthText(){
+
+  mytext.textContent = "Arceus healed itself";
+   
+ }
+
+  
  /* function damageText(){
     
     damageReceivedP1 = 100 - (healthP1.value)
@@ -238,63 +266,70 @@ function spacialRend(){
 }*/ 
 
 
-// Enemy Pokemon random attacks 
+// Enemy Pokemon random attack
 
-randomAttacks = [outrage,extremeSpeed,earthquake,recover];
+const opAttacks = ['outrage','outrage','shadow claw','shadow claw','earthquake','earthquake','recover'];
 
+let randomAttack;
 
+function randomOpAttack(){
 
+  randomAttack = opAttacks[Math.floor(Math.random() * opAttacks.length)];
+  if (randomAttack == 'outrage') {
+    outrage();
+   }
+   if (randomAttack == 'shadow claw') {
+    shadowClaw();
+   }
+   if (randomAttack == 'earthquake') {
+    earthquake();
+   }
+
+   if (randomAttack == 'recover') {
+    recover();
+   }
+  }
 
 // Different attacks  of Enemy Pokemon 
 
 function outrage(){
   mytext.textContent = `Arceus Used ${availablePokemon.Arceus.moves[0]}`;
   OutrageAudio.play();
-  setTimeout(opAttackText, DELAY_OPPONENT_ATTACK);
-  setTimeout(calculatOpDamage, DELAY_TAKE_DAMAGE);
+  setTimeout(superEffOpAttackText, DELAY_OPPONENT_ATTACK);
+  setTimeout(calculateSuperEffOpDamage, DELAY_TAKE_DAMAGE);
  // setTimeout(damageText, 4800);
   setTimeout(resetTextAndButtons, DELAY_RESET_TEXT);
-
-     }
+   }
  
- function extremeSpeed(){
+ function shadowClaw(){
   mytext.textContent =`Arceus Used ${availablePokemon.Arceus.moves[1]}`;
   //dracoMeteorAudio.play();
   setTimeout(attackText, DELAY_DRACO_METEOR);
-  setTimeout(calculatOpDamage, DELAY_TAKE_DAMAGE);
+  setTimeout(calculateOpDamage, DELAY_TAKE_DAMAGE);
  // setTimeout(damageText, 4800);
   setTimeout(resetTextAndButtons, DELAY_RESET_TEXT);
-  fightbtn.classList.add("fightButtonHidden");
-  switchbtn.classList.add("switchButtonHidden");
-  medicinebtn.classList.add("medicineButtonHidden");
-  escapebtn.classList.add("escapeButtonHidden");
+
  }
 
   function earthquake(){
   mytext.textContent = `Arceus Used ${availablePokemon.Arceus.moves[2]}`;
   //thunderAudio.play();
   setTimeout(attackText, DELAY_DRACO_METEOR);
-  setTimeout(calculatOpDamage, DELAY_THUNDER_DAMAGE);
+  setTimeout(calculateOpDamage, DELAY_THUNDER_DAMAGE);
  // setTimeout(damageText, 4800);
   setTimeout(resetTextAndButtons, DELAY_RESET_TEXT);
-  fightbtn.classList.add("fightButtonHidden");
-  switchbtn.classList.add("switchButtonHidden");
-  medicinebtn.classList.add("medicineButtonHidden");
-  escapebtn.classList.add("escapeButtonHidden");
+
  }
 
 
  function recover(){
   mytext.textContent = `Arceus Used ${availablePokemon.Arceus.moves[3]}`;
   //hydroPumpAudio.play();
-  setTimeout(attackText, DELAY_DRACO_METEOR);
-  setTimeout(calculatOpDamage, DELAY_THUNDER_DAMAGE);
+  setTimeout(recoverHealthText, DELAY_RECOVERY_TEXT);
+  setTimeout(healOpHealth, DELAY_RECOVER);
  // setTimeout(damageText, 4800);
   setTimeout(resetTextAndButtons, DELAY_RESET_TEXT);
-  fightbtn.classList.add("fightButtonHidden");
-  switchbtn.classList.add("switchButtonHidden");
-  medicinebtn.classList.add("medicineButtonHidden");
-  escapebtn.classList.add("escapeButtonHidden");
+
  }
 
  // Reset text and buttons to out Of Fight Mode
